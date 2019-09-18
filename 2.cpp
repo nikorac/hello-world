d68f632699d52be3a9c7ec11c7999a9f3dbc21c1
nikorac <55491454+nikorac@users.noreply.github.com>
2019-09-18 13:53:08 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	//riješiti uporabom dvostrukog uvjetnog granjanja
+	int a;
+	cout<<"Upiši neki broj razlièit od 0";
+	cin>>a;
+	if(a<0){
+		cout<<"Broj"<<a<<"je manji od 0";
+	}
+	else{
+			cout<<"Broj"<<a<<"je veæi od 0";
+	}
+}
